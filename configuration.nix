befc83d25ff49da23da61c1c4594f9d8631accc4
Nicolas Froger <nicolas@kektus.xyz>
2021-07-23 16:39:22 +0200

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,8 @@
+{...}:
+{
+  users.users.nicolas = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    hashedPassword = "$6$2xUbJKyOp0o0Hn2k$mFqM7kCQwqAjwdtuRZRQDrGPZrxV6coKoEHiW0m7AwET6LI9WOn6yT6oVumVbF0dkzfzRQk2/m4vxt45DTlHY/";
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "My personal NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgsMaster.url = "github:NixOS/nixpkgs/master";
+    home-manager.url = "github:nix-community/home-manager";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgsMaster, home-manager, flake-utils }:
+    let
+      inherit (nixpkgs) lib;
+      inherit (flake-utils.lib) eachDefaultSystem defaultSystems;
+
+      nixpkgsFor = lib.genAttrs defaultSystems (system : import nixpkgs {
+        inherit system;
+      });
+
+      developEnv = eachDefaultSystem (system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ git ];
+        };
+      });
+
+      myConfig = {
+        nixosConfigurations.mercury = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+
+    in
+    developEnv // myConfig;
+}
