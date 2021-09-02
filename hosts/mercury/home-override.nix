ecff11c0b3b66033380e5da9fb9e25782d6726a3
Nicolas Froger <nicolas@kektus.xyz>
2021-09-03 00:07:59 +0200

    mercury: add sway home-override

---
+++ hosts/mercury/home-override.nix
@@ -0,0 +1,13 @@
+{
+  wayland.windowManager.sway = {
+    output = {
+      DP-1 = {
+        position = "0,0";
+        transform = "270";
+      };
+      HDMI-A-1 = {
+        position = "1080,500";
+      };
+    };
+  };
+}
