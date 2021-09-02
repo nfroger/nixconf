9fc942ccdc9077b30d7742ef41ea46ae92aefb65
Nicolas Froger <nicolas@kektus.xyz>
2021-09-02 18:49:11 +0200

    callisto: add sway config override

---
+++ hosts/callisto/home-override.nix
@@ -0,0 +1,29 @@
+let
+  genWorkspaceAssign = disp:
+    map (n: {
+      output = disp;
+      workspace = toString n;
+    });
+in
+{
+  wayland.windowManager.sway.config = {
+    output = {
+      DP-1 = {
+        position = "0 440";
+      };
+      DP-2 = {
+        position = "1920 440";
+      };
+      VGA-1 = {
+        position = "3840 0";
+        transform = "90";
+      };
+    };
+    workspaceOutputAssign = genWorkspaceAssign "DP-2" (builtins.genList (x: x + 1) 5)
+      ++ genWorkspaceAssign "DP-1" (builtins.genList (x: x + 6) 5) ++
+      [{
+        output = "VGA-1";
+        workspace = "11";
+      }];
+  };
+}
