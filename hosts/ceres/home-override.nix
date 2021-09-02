968ae64a92741ed4f8012e0d59294f0fdc1f95ff
Nicolas Froger <nicolas@kektus.xyz>
2021-09-03 00:06:41 +0200

    sway: add home overrides for hosts ceres and mars

--- hosts/ceres/home-override.nix
+++ hosts/ceres/home-override.nix
@@ -1,1 +1,9 @@
-{ }
+{
+  wayland.windowManager.sway = {
+    input = {
+      "2:7:SynPS/2_Synaptics_TouchPad" = {
+        natural_scroll = "enabled";
+      };
+    };
+  };
+}
--- hosts/mars/home-override.nix
+++ hosts/mars/home-override.nix
@@ -1,1 +1,15 @@
-{ }
+{
+  wayland.windowManager.sway = {
+    input = {
+      "1739:31251:SYNA2393:00_06CB:7A13_Touchpad" = {
+        natural_scroll = "enabled";
+        tap = "enabled";
+      };
+    };
+    output = {
+      eDP-1 = {
+        scale = "1.25";
+      };
+    };
+  };
+}
