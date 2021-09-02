41e7391cfbf1074b2d09188bd976fe20bcf26ec3
Nicolas Froger <nicolas@kektus.xyz>
2021-09-02 19:24:31 +0200

    home: add less variables for colors in man pages

--- home.nix
+++ home.nix
@@ -4,6 +4,13 @@
   home.sessionVariables = {
     EDITOR = "vim";
     VISUAL = "vim";
+    LESS_TERMCAP_mb = "$'\\e[1;32m'";
+    LESS_TERMCAP_md = "$'\\e[1;32m'";
+    LESS_TERMCAP_me = "$'\\e[0m'";
+    LESS_TERMCAP_se = "$'\\e[0m'";
+    LESS_TERMCAP_so = "$'\\e[01;33m'";
+    LESS_TERMCAP_ue = "$'\\e[0m'";
+    LESS_TERMCAP_us = "$'\\e[1;4;31m'";
   };
 
   wayland.windowManager.sway =
