3f8cf6335773595d269370b92b98a57ed03948d0
Nicolas Froger <nicolas@kektus.xyz>
2021-09-03 19:43:23 +0200

    common: add firefox wrap with policies and addons

--- common.nix
+++ common.nix
@@ -79,10 +79,68 @@
     ansible
     kubectl
     tmux
-    firefox
     thunderbird
     discord
     slack
+    (wrapFirefox firefox-unwrapped {
+      nixExtensions = [
+        (fetchFirefoxAddon {
+          name = "ublock-origin";
+          url = "https://addons.mozilla.org/firefox/downloads/file/3816867/ublock_origin-1.37.2-an+fx.xpi";
+          sha256 = "17jmd90d0ix9nm1n2jh4friq5bas6x88nm6x6c765d5cj4cci8xk";
+        })
+        (fetchFirefoxAddon {
+          name = "bitwarden";
+          url = "https://addons.mozilla.org/firefox/downloads/file/3831245/bitwarden_free_password_manager-1.52.1-an+fx.xpi";
+          sha256 = "1fjbkqb19c733xg4mar31a923zkcpiyk62hs3llwiasd7dfyvmlm";
+        })
+        (fetchFirefoxAddon {
+          name = "sponsorblock";
+          url = "https://addons.mozilla.org/firefox/downloads/file/3834324/sponsorblock_skip_sponsorships_on_youtube-3.0.5-an+fx.xpi";
+          sha256 = "19928ym85vswwkiz8hqrhc3fv977s0xjh6g7ppbhmdjj783lfy46";
+        })
+        (fetchFirefoxAddon {
+          name = "video-speed-controller";
+          url = "https://addons.mozilla.org/firefox/downloads/file/3756025/video_speed_controller-0.6.3.3-an+fx.xpi";
+          sha256 = "0ahhaapdxlj74pxb6zhw36m2glgv6xgm2c7gndd2pn8dabrjb8ny";
+        })
+      ];
+
+      extraPolicies = {
+        CaptivePortal = false;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisableFirefoxAccounts = true;
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
+        FirefoxHome = {
+          Search = false;
+          TopSites = false;
+          Highlights = false;
+          Pocket = false;
+          Snippets = false;
+        };
+        Homepage = {
+          StartPage = "none";
+        };
+        NewTabPage = false;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        OverrideFirstRunPage = "";
+        PasswordManagerEnabled = false;
+        Bookmarks = [
+          {
+            Title = "Google";
+            URL = "https://google.com";
+            Favicon = "https://google.com/favicon.ico";
+            Placement = "toolbar";
+          }
+        ];
+      };
+    })
   ];
 
   programs.neovim.vimAlias = true;
