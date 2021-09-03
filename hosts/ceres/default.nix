b0244fe7db31eb22eb5e5817d551d11bbe3e4d85
Nicolas Froger <nicolas@kektus.xyz>
2021-09-03 11:29:51 +0200

    ceres: add mountpoints template

--- hosts/ceres/default.nix
+++ hosts/ceres/default.nix
@@ -5,7 +5,34 @@
     ../../common.nix
   ];
 
+  boot.initrd.kernelModules = [ "dm-crypt" ];
+
   networking.hostName = "ceres";
 
+  boot.initrd.luks.devices = {
+    cryptlvm = {
+      device = "/dev/disk/by-uuid/80e74efd-3642-4db1-9676-ca4e980aa34a";
+      preLVM = false;
+    };
+  };
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/FIXME";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/FIXME";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/FIXME"; }];
+
+
   kektus.laptop.enable = true;
+
+  system.stateVersion = "21.05";
 }
