dfe28a4fe6bc2156f53f95de37b9afe68bdccfd6
Nicolas Froger <nicolas@kektus.xyz>
2021-09-03 19:08:45 +0200

    home: sway: fix swaylock shortcut

--- home.nix
+++ home.nix
@@ -89,7 +89,7 @@
           lib.mkOptionDefault
             {
               "${modifier}+Delete" = "mode \"${sysmenu}\"";
-              "${modifier}+L" = "exec swaylock -F -e -l -c 212121 --indicator-idle-visible";
+              "${modifier}+l" = "exec swaylock -F -e -l -c 212121 --indicator-idle-visible";
               Print = "exec ${pkgs.grim}/bin/grim -g \"$(${pkgs.slurp}/bin/slurp)\" - | wl-copy";
             };
       };
