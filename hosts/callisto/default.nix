a5fdcb43056505778de2e2ef269151715cdba56a
Nicolas Froger <nicolas@kektus.xyz>
2021-09-03 18:28:46 +0200

    callisto: add openafs client

--- hosts/callisto/default.nix
+++ hosts/callisto/default.nix
@@ -105,5 +105,20 @@
     };
   };
 
+  services.openafsClient = {
+    enable = true;
+    cellName = "cri.epita.fr";
+    cellServDB = [
+      { ip = "10.224.21.100"; dnsname = "afs-0.pie.cri.epita.fr"; }
+      { ip = "10.224.21.101"; dnsname = "afs-1.pie.cri.epita.fr"; }
+      { ip = "10.224.21.102"; dnsname = "afs-2.pie.cri.epita.fr"; }
+      { ip = "10.224.4.107"; dnsname = "storage-3.pie.cri.epita.fr"; }
+    ];
+    cache = {
+      diskless = true;
+    };
+    fakestat = true;
+  };
+
   system.stateVersion = "21.05";
 }
