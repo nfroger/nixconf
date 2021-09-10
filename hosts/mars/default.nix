4bd9e4de7a4148b253de79f34582221f150f3e16
Nicolas Froger <nicolas@kektus.xyz>
2021-09-10 10:21:59 +0200

    hosts: mars: add disks and various configs

--- hosts/mars/default.nix
+++ hosts/mars/default.nix
@@ -1,10 +1,45 @@
+{ nixos-hardware, ... }:
+
 {
-  # Dell XPS 15
+  # Dell XPS 15 9570
 
   imports = [
     ../../profiles/core.nix
     ../../profiles/laptop.nix
+    nixos-hardware.nixosModules.common-pc-laptop
+    nixos-hardware.nixosModules.common-pc-laptop-ssd
+    nixos-hardware.nixosModules.dell-xps-15-9560-intel
   ];
 
   networking.hostName = "mars";
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" "dm-crypt" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  hardware.enableRedistributableFirmware = true;
+
+  boot.initrd.luks.devices = {
+    cryptlvm = {
+      device = "/dev/disk/by-uuid/a599cb75-a689-4e78-9bce-73415ce867bd";
+      preLVM = true;
+    };
+  };
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/f8e34f92-fadb-4890-9549-843319df82d0";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/C342-AE9F";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/94319718-a960-4104-bb63-818845cefb9f"; }];
+
+  system.stateVersion = "21.05";
 }
