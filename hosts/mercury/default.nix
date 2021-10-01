910ae7f80020f7816c9b12e4b28f2667e455353b
Nicolas Froger <nicolas@kektus.xyz>
2021-10-01 19:27:37 +0200

    hosts: mercury: remove raid1 and lvm thin, update disks

--- hosts/mercury/default.nix
+++ hosts/mercury/default.nix
@@ -18,7 +18,7 @@
   };
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ "dm-snapshot" "dm-mod" "dm-cache" "dm-cache-smq" "dm-thin-pool" "raid1" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" "dm-mod" "dm-crypt" ];
   boot.kernelModules = [ "kvm-intel" "vfio_pci" "vfio" "vfio_iommu_type1" "vfio_virqfd" ];
   boot.extraModulePackages = [ ];
   boot.kernelParams = [ "intel_iommu=on" "iommu=pt" "vfio-pci.ids=10de:13c2,10de:0fbb" ];
@@ -30,42 +30,27 @@
     "net.ipv4.ip_forward" = 1;
   };
 
-  boot.initrd.extraUtilsCommands = ''
-    for BIN in ${pkgs.thin-provisioning-tools}/{s,}bin/*; do
-        copy_bin_and_libs $BIN
-    done
-  '';
-  # Before LVM commands are executed, ensure that LVM knows exactly where our cache and thin provisioning tools are
-  boot.initrd.preLVMCommands = ''
-    mkdir -p /etc/lvm
-    echo "global/thin_check_executable = "$(which thin_check)"" >> /etc/lvm/lvm.conf
-    echo "global/cache_check_executable = "$(which cache_check)"" >> /etc/lvm/lvm.conf
-    echo "global/cache_dump_executable = "$(which cache_dump)"" >> /etc/lvm/lvm.conf
-    echo "global/cache_repair_executable = "$(which cache_repair)"" >> /etc/lvm/lvm.conf
-    echo "global/thin_dump_executable = "$(which thin_dump)"" >> /etc/lvm/lvm.conf
-    echo "global/thin_repair_executable = "$(which thin_repair)"" >> /etc/lvm/lvm.conf
-  '';
-
-  boot.initrd.mdadmConf = ''
-    ARRAY /dev/md/Raid1Lvm  metadata=1.2 UUID=7e4af654:59346547:b73345ac:ea3002d3 name=any:Raid1Lvm
-    ARRAY /dev/md/Raid1Swap  metadata=1.2 UUID=6b2e5afc:43e07a13:eba3cbb5:8441e5d7 name=any:Raid1Swap
-  '';
-  services.lvm.boot.thin.enable = true;
+  boot.initrd.luks.devices = {
+    cryptlvm = {
+      device = "/dev/disk/by-uuid/9a83d0df-18bc-473d-928e-5a3d4f3149b4";
+      preLVM = false;
+    };
+  };
 
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/d1b43f5d-1d46-4517-8a86-ee45abd266bd";
+      device = "/dev/disk/by-uuid/4157cd65-3e83-46cf-9ab7-cb11b158a842";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/03DA-0074";
+      device = "/dev/disk/by-uuid/4532-1F9D";
       fsType = "vfat";
     };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/52780eb3-01ea-4ed4-928d-b79da50225f2"; }];
+    [{ device = "/dev/disk/by-uuid/6d9b9de5-dd5a-44d7-989d-562f36f20b8a"; }];
 
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemuOvmf = true;
@@ -83,5 +68,5 @@
     ]
   '';
 
-  system.stateVersion = "20.09";
+  system.stateVersion = "21.11";
 }
