eb2411fca7d51590fb6f153acd37555436e77650
Nicolas Froger <nicolas@kektus.xyz>
2021-10-06 22:47:41 +0200

    profiles: core: printing: remove brlaser driver

--- profiles/core.nix
+++ profiles/core.nix
@@ -50,7 +50,6 @@
   services.openssh.enable = true;
   services.pcscd.enable = true; # Smartcard support
   services.printing.enable = true;
-  services.printing.drivers = [ pkgs.brlaser ];
 
   virtualisation.libvirtd.enable = true;
   virtualisation.libvirtd.qemuOvmf = true;
