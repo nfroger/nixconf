3306909b4f6c06d29ced3b7820dcf3e938a0b9f0
Nicolas Froger <nicolas@kektus.xyz>
2022-06-03 12:14:19 +0200

    pkgs: remove vscodium and discord overrides
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1,10 +1,1 @@
-{
-  discord = {
-    path = ../applications/networking/instant-messengers/discord;
-    args = final: prev: { inherit (prev) discord; };
-  };
-  vscodium = {
-    path = ../applications/editors/vscode/vscodium.nix;
-    args = final: prev: { inherit (prev) vscodium; };
-  };
-}
+{ }
