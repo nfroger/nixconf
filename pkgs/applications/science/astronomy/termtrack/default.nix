f161b098420362d52d1ecc6cc520181b885948ea
Nicolas Froger <nicolas@kektus.xyz>
2022-11-08 17:07:48 +0100

    packages: add termtrack
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

---
+++ pkgs/applications/science/astronomy/termtrack/default.nix
@@ -0,0 +1,30 @@
+{ lib, buildPythonPackage, fetchPypi, click, pillow, ephem, pyshp, requests }:
+
+buildPythonPackage rec {
+  pname = "termtrack";
+  version = "0.7.4";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-d9Y3wFppaOLhKtLiXEwbjIx8eIKdHYTTPnttqPUlfV8=";
+  };
+
+  propagatedBuildInputs = [
+    click
+    pillow
+    ephem
+    pyshp
+    requests
+  ];
+
+  preBuild = ''
+    substituteInPlace setup.py --replace pyephem ephem
+
+  '';
+
+  meta = with lib; {
+    homepage = "https://github.com/trehn/termtrack";
+    description = "Track satellites in your terminal";
+    license = licenses.gpl3;
+  };
+}
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1,1 +1,6 @@
-{ }
+{
+  termtrack = {
+    path = ../applications/science/astronomy/termtrack;
+    callPackage = final: prev: final.python3Packages.callPackage;
+  };
+}
