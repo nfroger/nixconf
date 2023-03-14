2be27b3c24d72da6dd9354422a6e6af241092bb6
Nicolas Froger <nicolas@kektus.xyz>
2023-03-14 18:27:59 +0100

    hosts: callisto: remove AFS servers
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- hosts/callisto/default.nix
+++ hosts/callisto/default.nix
@@ -140,11 +140,6 @@
   services.openafsClient = {
     enable = true;
     cellName = "cri.epita.fr";
-    cellServDB = [
-      { ip = "10.224.21.100"; dnsname = "afs-0.pie.cri.epita.fr"; }
-      { ip = "10.224.21.101"; dnsname = "afs-1.pie.cri.epita.fr"; }
-      { ip = "10.224.21.102"; dnsname = "afs-2.pie.cri.epita.fr"; }
-    ];
     cache = {
       diskless = true;
     };
