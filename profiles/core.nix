0e5ffd37ff87c1c83d25323020fe7c57230c406c
Nicolas Froger <nicolas@kektus.xyz>
2023-03-16 15:21:35 +0100

    profiles: core: enable wireshark
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- profiles/core.nix
+++ profiles/core.nix
@@ -10,7 +10,7 @@
 
   users.users.nicolas = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirtd" "input" "kvm" "docker" "video" "dialout" "ubridge" ];
+    extraGroups = [ "wheel" "libvirtd" "input" "kvm" "docker" "video" "dialout" "ubridge" "wireshark" ];
     shell = pkgs.zsh;
     hashedPassword = "$6$2xUbJKyOp0o0Hn2k$mFqM7kCQwqAjwdtuRZRQDrGPZrxV6coKoEHiW0m7AwET6LI9WOn6yT6oVumVbF0dkzfzRQk2/m4vxt45DTlHY/";
   };
@@ -93,6 +93,7 @@
     enable = true;
     enableSSHSupport = true;
   };
+  programs.wireshark.enable = true;
 
   networking.wireguard.enable = true;
 
