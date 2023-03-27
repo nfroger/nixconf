6edb351c7dde7320797254ad5061971beb3b41cf
Nicolas Froger <nicolas@kektus.xyz>
2023-03-27 11:21:18 +0200

    home: packages: add pieVNC and vinagre
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- home/packages.nix
+++ home/packages.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
-
+let
+  pieVNC = pkgs.writeScriptBin "pieVNC" ''
+    ssh -S none -t -L 5900:localhost:5900 "root@$1" 'x11vnc -auth /home/$(loginctl | grep -v tty | grep seat0 | awk '\'''{ print $3 }'\''')/.Xauthority -localhost -display :0'
+  '';
+in
 {
   home.packages = with pkgs; [
     (python310.withPackages (p: with p; [ ipython poetry black ]))
@@ -8,20 +12,22 @@
     awscli2
     discord
     dynamips
-    gnumeric
+    gnome.vinagre
     gns3-gui
     gns3-server
+    gnumeric
     imagemagick
     inetutils
-    jetbrains.idea-ultimate
-    maven
     jdk
+    jetbrains.idea-ultimate
     jq
     libreoffice
+    maven
     minio-client
     openldap
     openssl
     packer
+    pieVNC
     slack
     spotify
     teams
