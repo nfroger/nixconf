41ee9ccbea84834c74a4144ef29f3900001741a8
Nicolas Froger <nicolas@kektus.xyz>
2023-04-19 10:59:24 +0200

    hosts: add ganymede
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

---
+++ hosts/ganymede/default.nix
@@ -0,0 +1,47 @@
+{ pkgs, nixos-hardware, ... }:
+
+{
+  # Lenovo Thinkpad L14
+
+  imports = [
+    ../../profiles/core.nix
+    ../../profiles/laptop.nix
+    ../../profiles/docker.nix
+    nixos-hardware.nixosModules.common-pc-laptop
+    nixos-hardware.nixosModules.common-pc-laptop-ssd
+    nixos-hardware.nixosModules.lenovo-thinkpad-l14-amd
+  ];
+
+  networking.hostName = "ganymede";
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" "dm-crypt" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+  hardware.enableRedistributableFirmware = true;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_2;
+
+  boot.initrd.luks.devices = {
+    cryptlvm = {
+      device = "/dev/disk/by-uuid/4cfb4eec-3dd6-491c-840b-1e8bb0b36756";
+      preLVM = true;
+    };
+  };
+
+  fileSystems."/" =
+    {
+      device = "/dev/disk/by-uuid/e85c21ef-8da2-4af7-ac7f-83c30a8d7e10";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/disk/by-uuid/698B-CB82";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [{ device = "/dev/disk/by-uuid/75a10f00-4e55-4389-8d54-51e83f23fae7"; }];
+
+  system.stateVersion = "22.11";
+}
---
+++ hosts/ganymede/home-override.nix
@@ -0,0 +1,55 @@
+{
+  wayland.windowManager.sway.config = {
+    input = {
+      "1267:12809:ELAN067F:00_04F3:3209_Touchpad" = {
+        natural_scroll = "enabled";
+        tap = "enabled";
+      };
+    };
+    output = {
+      eDP-1 = {
+        scale = "1.25";
+      };
+    };
+  };
+
+  services.kanshi.profiles = {
+    normal = {
+      outputs = [
+        {
+          criteria = "eDP-1";
+          status = "enable";
+        }
+      ];
+    };
+    dockedHome = {
+      outputs = [
+        {
+          criteria = "eDP-1";
+          status = "disable";
+        }
+        {
+          criteria = "BenQ Corporation BenQ XL2411Z 6AF01007SL0";
+          position = "1080,470";
+        }
+        {
+          criteria = "BenQ Corporation BenQ GW2480 ETV5L03568SL0";
+          position = "0,0";
+          transform = "90";
+        }
+      ];
+    };
+    dockedOffice = {
+      outputs = [
+        {
+          criteria = "eDP-1";
+          status = "disable";
+        }
+        {
+          criteria = "Lenovo Group Limited LEN";
+          scale = 1.25;
+        }
+      ];
+    };
+  };
+}
