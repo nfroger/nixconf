a2f54361b2fcfb2b27049a8a5f77db1228122df0
Nicolas Froger <nicolas@kektus.xyz>
2024-06-03 11:04:10 +0200

    hosts: callisto: update display config
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- hosts/callisto/home-override.nix
+++ hosts/callisto/home-override.nix
@@ -10,23 +10,15 @@ in
 {
   wayland.windowManager.sway.config = {
     output = {
-      VGA-1 = {
+      DP-1 = {
         position = "0 0";
-        transform = "270";
       };
       DP-2 = {
-        position = "1080 420";
-      };
-      DP-1 = {
-        position = "3000 420";
+        position = "1920 0";
       };
     };
-    workspaceOutputAssign = genWorkspaceAssign "DP-2" (builtins.genList (x: x + 1) 5)
-      ++ genWorkspaceAssign "DP-1" (builtins.genList (x: x + 6) 5) ++
-      [{
-        output = "VGA-1";
-        workspace = "11";
-      }];
+    workspaceOutputAssign = genWorkspaceAssign "DP-1" (builtins.genList (x: x + 1) 5)
+      ++ genWorkspaceAssign "DP-2" (builtins.genList (x: x + 6) 5);
   };
 
   programs.ssh.matchBlocks."fw-cri".hostname = lib.mkForce "10.201.5.2";
