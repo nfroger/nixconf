02c963853910e4c01b2009a8bbe4844688376a32
Nicolas Froger <nicolas@kektus.xyz>
2024-09-10 22:56:52 +0200

    profiles: gaming: add mangohud
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- profiles/gaming.nix
+++ profiles/gaming.nix
@@ -5,17 +5,20 @@
     enable = true;
     extraPackages = with pkgs; [
       gamescope
+      mangohud
       xorg.libXcursor
       xorg.libXi
       xorg.libXinerama
       xorg.libXScrnSaver
       libpng
       libpulseaudio
+      pipewire
       libvorbis
       stdenv.cc.cc.lib
       libkrb5
       keyutils
     ];
+    gamescopeSession.enable = true;
   };
   programs.gamescope = {
     enable = true;
