2fb6c429376d8ca4294abbc740f4e180b4952d14
Nicolas Froger <nicolas@kektus.xyz>
2025-08-15 23:39:55 +0200

    home: fix kubeswitch config
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- home/kubecli-packages.nix
+++ home/kubecli-packages.nix
@@ -20,4 +20,22 @@
     kubent
     kube-capacity
   ];
+
+  programs.zsh.initContent =
+    let
+      kubeswitchZshFiles =
+        pkgs.runCommand "kubeswitch-shell-files-for-zsh"
+          {
+            nativeBuildInputs = [ pkgs.kubeswitch ];
+          }
+          ''
+            mkdir -p $out/share
+            switcher init zsh | sed "s/switch(/kubeswitch(/" > "$out/share/kubeswitch_init.zsh"
+            switcher --cmd kubeswitch completion zsh > "$out/share/kubeswitch_completion.zsh"
+          '';
+    in
+    ''
+      source ${kubeswitchZshFiles}/share/kubeswitch_init.zsh
+      source ${kubeswitchZshFiles}/share/kubeswitch_completion.zsh
+    '';
 }
