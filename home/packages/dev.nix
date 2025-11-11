58d1e65304c4cf2e13d9fbf74f044b532b440652
Nicolas Froger <nicolas@kektus.xyz>
2025-11-11 17:29:48 +0100

    home: dev: add UV settings
    Signed-off-by: Nicolas Froger <nicolas@kektus.xyz>

--- home/packages/dev.nix
+++ home/packages/dev.nix
@@ -1,14 +1,22 @@
 { pkgs, ... }:
+let
+  pythonPkg = pkgs.python3.withPackages (
+    p: with p; [
+      ipython
+      black
+    ]);
+in
 {
   home.packages = with pkgs; [
-    (python3.withPackages (
-      p: with p; [
-        ipython
-        black
-      ]
-    ))
+    pythonPkg
     maven
     poetry
     uv
   ];
+
+  home.sessionVariables = {
+    UV_NO_SYNC = "1";
+    UV_PYTHON = "${pythonPkg}/bin/python";
+    UV_PYTHON_DOWNLOADS = "never";
+  };
 }
